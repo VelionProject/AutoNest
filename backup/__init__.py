580eacb6034b0693c4d8fc7cb0a9c08b7ae95b02
Solen <Velion.project@gmail.com>
2025-06-07 21:53:01 +0200

    Export public APIs in package init

--- backup/__init__.py
+++ backup/__init__.py
@@ -1,1 +1,25 @@
+"""Backup utilities for AutoNest."""
 
+from .backup_manager import (
+    create_backup_session,
+    backup_file_to_session,
+    list_backup_sessions,
+    restore_file_from_session,
+)
+from .memory_context_manager import (
+    get_memory_path,
+    save_context_entry,
+    load_recent_entries,
+    clear_memory,
+)
+
+__all__ = [
+    "create_backup_session",
+    "backup_file_to_session",
+    "list_backup_sessions",
+    "restore_file_from_session",
+    "get_memory_path",
+    "save_context_entry",
+    "load_recent_entries",
+    "clear_memory",
+]
