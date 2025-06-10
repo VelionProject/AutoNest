a95a8024d044e88d400d9c7c968f1456af389028
Solen <Velion.project@gmail.com>
2025-06-10 20:35:38 +0200

    feat(gui): add sidebar interface and logs

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [0.3] - 2024-??-??
+### Added
+- Revamped GUI with sidebar navigation, logs panel, and project settings.
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autonest",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=["openai", "requests"],
     entry_points={"console_scripts": ["autonest-gui=interface.autonest_gui:main"]},
